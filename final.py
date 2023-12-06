68738985f95297eabbfec061734f9c96ed9518fd
awinnnie <114687525+awinnnie@users.noreply.github.com>
2023-12-06 20:58:36 +0400

    Update final.py

--- final.py
+++ final.py
@@ -1,14 +1,13 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 from Reading_the_data import *
 from Data_sanitazation import *
+from plotting import *
 
 # Reading the data
 table = read_data('marksheet.csv')
 print(table)
 
-# Removing unrelated columns
-table = clean_columns(table)
+# # Removing unrelated columns
+# table = clean_columns(table)
 
 # Missing values
 miss_val = missing_values(table)
@@ -28,89 +27,20 @@ print(f"\nDuplicate Rows Removed: {dup_removed}")
 print(f"\nCleaned Dataset Statistics:")
 print(table.info())  #information about the cleaned dataframe
 
-
-
-# Plotting mean, high, low
-
 subjects = ["Science", "English", "History", "Maths"] #encloses the needed columns for average grade calculation
 table["mean"] = table[subjects].mean(axis = 1) #Calculates the average of grades for each student
-High_Performers = []
-Low_Performers = []
-Mid_Performers = []
-for i, j in table.iterrows():
-    if j['mean']>= 85:
-        High_Performers.append(j['Name'])
-    if j['mean']<=40:
-        Low_Performers.append(j["Name"])
-    if j['mean']>=60 and j['mean']<=85:
-        Mid_Performers.append(j["Name"])
 
-def y_values(list):
-    '''This function returns a list with the average grades of students which are passed to the function in a list'''
-    y_values = []
-    for index, row in table.iterrows():
-        if row['Name'] in list:
-            y_values.append(row["mean"])
-    return(y_values)
+# Plotting high, low, mid performances
+plot_high_low(table)
 
-fig, ax = plt.subplots(figsize = (8, 10)) #unpacks the subplot into its' axes. This is a general syntax for creating figures
+# Plotting based on sections
+plot_sections(table)
 
-# Plot high performers in a horizontal bar chart. The first argument creates the y_values(each student)
-ax.barh(High_Performers,y_values(High_Performers), color='green', label='High Performers')
+# Plotting based on subjects
+plot_subjects(table)
 
-#Likewise plot Mid performers
-ax.barh(Mid_Performers,y_values(Mid_Performers), color='yellow', label='Mid Performers')
+# Plotting based on gender
+plot_gender(table)
 
-# Plot low performers
-ax.barh(Low_Performers, y_values(Low_Performers), color='red', label='Low Performers')
-
-# Customizing the plot
-ax.set_yticks(range(len(y_values(High_Performers)) + len(y_values(Low_Performers))+len(y_values(Mid_Performers)))) #Makes sure there are spaces for each label on the Y axis
-ax.set_xlabel('Grade Mean') #Shows the label of X axis
-ax.set_title('High, Low and Mid Performers in Class')
-plt.tight_layout() #adjusts the layout so that the names do not overlap
-ax.legend() #Shows the legend
-
-# Display the plot
-plt.show()
-
-
-
-# Plotting means by subject
-
-fig, ax = plt.subplots()
-subjects = ["Science", "English", "History", "Maths"]
-subject_means = table[['Science', 'English', 'History', 'Maths']].mean()
-plt.bar(subjects, subject_means)
-plt.title("the means of the subjects")
-plt.show()
-
-# Plotting means by sections
-
-Sum_A = 0
-Sum_B = 0
-Sum_C = 0
-Count_A = 0
-Count_B = 0
-Count_C = 0
-for i, j in table.iterrows():
-    count = 0
-    if j['Section'] == "A":
-        Count_A += 1
-        Sum_A += j["mean"]
-    elif j['Section'] == "B":
-        Count_B += 1
-        Sum_B += j["mean"]
-    else:
-        Count_C += 1
-        Sum_C += j["mean"]
-
-Mean_A = Sum_A/Count_A
-Mean_B = Sum_B/Count_B
-Mean_C = Sum_C/Count_C
-Sections = ['A', 'B', 'C']
-Means = [Mean_A, Mean_B, Mean_C]
-fig, ax = plt.subplots()
-plt.bar(Sections, Means)
-plt.title("Means based on Sections")
-plt.show()
+# Plotting based on age
+plot_age_performance(table)
