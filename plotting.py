b527c07cdc0e46882bbde8062f12d9577c9cc812
minasyannat <123963013+minasyannat@users.noreply.github.com>
2023-12-13 23:22:38 +0400

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -8,6 +8,7 @@ subjects = ["Science", "English", "History", "Maths"] #encloses the needed colum
 table["mean"] = table[subjects].mean(axis = 1) #Calculates the average of grades for each student
 
 def plot_high_low(table):
+    """This function plots high, mid and low performers taking the dataframe as an argument"""
     High_Performers = []
     Low_Performers = []
     Mid_Performers = []
@@ -31,26 +32,27 @@ def plot_high_low(table):
 
     fig, ax = plt.subplots(figsize = (8, 10)) #unpacks the subplot into its' axes. This is a general syntax for creating figures
 
-    # Plot high performers in a horizontal bar chart. The first argument creates the y_values(each student)
+    # Plots high performers in a horizontal bar chart. The first argument creates the y_values(each student)
     ax.barh(High_Performers,y_values(High_Performers), color='green', label='High Performers')
 
-    #Likewise plot Mid performers
+    #Likewise plots Mid performers
     ax.barh(Mid_Performers,y_values(Mid_Performers), color='yellow', label='Mid Performers')
 
-    # Plot low performers
+    # Plots low performers
     ax.barh(Low_Performers, y_values(Low_Performers), color='red', label='Low Performers')
 
-    # Customizing the plot
+    # Some customization for the plot
     ax.set_yticks(range(len(y_values(High_Performers)) + len(y_values(Low_Performers))+len(y_values(Mid_Performers)))) #Makes sure there are spaces for each label on the Y axis
     ax.set_xlabel('Grade Mean') #Shows the label of X axis
     ax.set_title('High, Low and Mid Performers in Class')
     plt.tight_layout() #adjusts the layout so that the names do not overlap
     ax.legend() #Shows the legend
 
-    # Display the plot
+    # Displays the plot
     plt.show()
     
 def plot_subjects(table):
+    """Plots a bar chart comparing the performance based on the subjects. Takes the dataframe as an argument."""
     fig, ax = plt.subplots()
     subjects = ["Science", "English", "History", "Maths"]
     subject_means = table[['Science', 'English', 'History', 'Maths']].mean()
@@ -59,6 +61,7 @@ def plot_subjects(table):
     plt.show()
     
 def plot_sections(table):
+    """Plots the performance based on the sections. Calculates the average grades for each section then plots it as a bar chart, takes the dataframe as an argument"""
     Sum_A = 0
     Sum_B = 0
     Sum_C = 0
@@ -88,6 +91,7 @@ def plot_sections(table):
     plt.show()
     
 def plot_gender(table):
+    """compares the average grade for math based on gender in a bar chart"""
     F_mean = 0
     M_mean = 0
     F_math = []
@@ -110,9 +114,11 @@ def plot_gender(table):
     plt.show()
     
 def plot_age_performance(table):
+    """Creates a scatter plot which correlates the performance with age"""
     def age_calc(birthdate):
+        """Calculates the exact age based on the birthdate"""
         today = datetime.today()
-        birthdate = datetime.strptime(birthdate, '%m/%d/%Y') #this is a standard syntax for "unpacking" the string input into a year- month - day datetime format
+        birthdate = datetime.strptime(birthdate, '%m/%d/%Y') #this is a standard syntax for "unpacking" the string input into a month- day - year datetime format
         age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
         return age
 
