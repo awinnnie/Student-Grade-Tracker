3b7764292cbdc5a562f319e4237c73e0484c1fee
awinnnie <114687525+awinnnie@users.noreply.github.com>
2023-12-14 16:44:56 +0400

    Update Data_sanitazation.py

--- Data_sanitazation.py
+++ Data_sanitazation.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
 def missing_values(table):
+    """Calculates the number of missing values in each column of the file."""
     missing_values = table.isna().sum()
     return missing_values
 
-def missing_to_zero(table)  : 
+def missing_to_zero(table): 
+    """Replaces the missing values with zeros."""
     table = table.fillna(0)
     return table
 
 def clean_columns(table):
+    """Removes specific columns ('Gender','Section')"""
     table.drop('Gender', inplace=True, axis=1)
     table.drop('Section', inplace=True, axis=1)
     return table 
 
 def remove_dup(table):
+    """Removes duplicate rows and returns the number of them"""
     before_dup = len(table)
     table.drop_duplicates(inplace=True)
     after_dup = len(table)
@@ -22,4 +26,5 @@ def remove_dup(table):
     return dup_removed
 
 def to_string(table):
+    """COnverts specific columns ('Name', 'Gender', 'Section') to string data type"""
     table[["Name", "Gender", "Section"]] = table[["Name", "Gender", "Section"]].astype('string')
